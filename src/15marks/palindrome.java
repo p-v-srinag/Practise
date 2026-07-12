d8f4b119a9326f605744427ad222d8a2402bc517
Polu Venkata Sri Nag <138875681+p-v-srinag@users.noreply.github.com>
2026-07-12 18:17:13 +0530

    Implement palindrome check with two-pointer method
    Added a method to check if a string is a palindrome using a two-pointer approach.

--- src/15marks/palindrome.java
+++ src/15marks/palindrome.java
@@ -18,3 +18,34 @@ class palindrome
 
     }
 }
+/* for string 2 pointer approch*/
+import java.util.*;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.print(ispalendrome(s));
+    }
+    public static boolean ispalendrome(String s)
+    {
+        if(s==null)
+        {
+            return false;
+        }
+        s=s.toLowerCase();
+        int left = 0;
+        int right=s.length()-1;
+        while(left<right)
+        {
+            if(s.charAt(left)!=s.charAt(right))
+            {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
